15a8e96f16fcefb082ba4e971847ae51767e785b
thde <thde@users.noreply.github.com>
2023-09-26 16:52:41 +0200

    ci: use github actions

--- lib/puppet-lint/plugins/check_global_resource.rb
+++ lib/puppet-lint/plugins/check_global_resource.rb
@@ -21,23 +21,23 @@ PuppetLint.new_check(:global_resource) do
       next if @secure.any? { |s| s[0] < r[:start] && s[1] > r[:end] }
 
       notify :error,
-             message: "Resource #{r[:type].value} in global space",
-             line: r[:type].line,
-             column: r[:type].column
+        message: "Resource #{r[:type].value} in global space",
+        line: r[:type].line,
+        column: r[:type].column
     end
   end
 
   def check_for_global_includes
     tokens.each_index do |i|
       token = tokens[i]
-      next unless token.type == :NAME && token.value == 'include'
+      next unless token.type == :NAME && token.value == "include"
 
       next if @secure.any? { |s| s[0] < i && s[1] > i }
 
       notify :error,
-             message: "include #{token.next_code_token.value} in global space",
-             line: token.line,
-             column: token.column
+        message: "include #{token.next_code_token.value} in global space",
+        line: token.line,
+        column: token.column
     end
   end
 end
--- spec/puppet-lint/plugins/check_global_resource_spec.rb
+++ spec/puppet-lint/plugins/check_global_resource_spec.rb
@@ -1,44 +1,44 @@
-require 'spec_helper'
+require "spec_helper"
 
-describe 'global_resource' do
-  context 'just a class' do
+describe "global_resource" do
+  context "just a class" do
     let(:code) { "class test { file { 'file': } }" }
 
-    it 'should not detect any problems' do
+    it "should not detect any problems" do
       expect(problems).to have(0).problems
     end
   end
 
-  context 'just a define' do
+  context "just a define" do
     let(:code) { "define test ($param = undef) { file { 'file': } }" }
 
-    it 'should not detect any problems' do
+    it "should not detect any problems" do
       expect(problems).to have(0).problems
     end
   end
 
-  context 'allow node resources' do
+  context "allow node resources" do
     let(:code) { "node 'test' { file { 'file': } }" }
 
-    it 'should not detect any problems' do
+    it "should not detect any problems" do
       expect(problems).to have(0).problems
     end
   end
 
-  context 'global file' do
+  context "global file" do
     let(:code) do
       "file { 'file': } define test ($param = undef) { file { 'file': } }"
     end
 
-    it 'should detect a problem' do
+    it "should detect a problem" do
       expect(problems).to have(1).problems
     end
   end
 
-  context 'global includes' do
+  context "global includes" do
     let(:code) { "class test { file { 'file': } } \ninclude class" }
 
-    it 'should detect a problem' do
+    it "should detect a problem" do
       expect(problems).to have(1).problems
     end
   end
