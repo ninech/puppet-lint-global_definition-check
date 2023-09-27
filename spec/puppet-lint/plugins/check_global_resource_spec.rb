1407b1e2f464a8521ef501cafa8939ce1a3c9ee9
thde <thde@users.noreply.github.com>
2023-09-27 11:24:22 +0200

    feat: split checks

--- spec/puppet-lint/plugins/check_global_resource_spec.rb
+++ spec/puppet-lint/plugins/check_global_resource_spec.rb
@@ -1,48 +1,6 @@
 require "spec_helper"
 
-describe "global_resource" do
-  context "just a class" do
-    let(:code) { "class test { file { 'file': } }" }
-
-    it "should not detect any problems" do
-      expect(problems).to have(0).problems
-    end
-  end
-
-  context "just a define" do
-    let(:code) { "define test ($param = undef) { file { 'file': } }" }
-
-    it "should not detect any problems" do
-      expect(problems).to have(0).problems
-    end
-  end
-
-  context "allow node resources" do
-    let(:code) { "node 'test' { file { 'file': } }" }
-
-    it "should not detect any problems" do
-      expect(problems).to have(0).problems
-    end
-  end
-
-  context "global file" do
-    let(:code) do
-      "file { 'file': } define test ($param = undef) { file { 'file': } }"
-    end
-
-    it "should detect a problem" do
-      expect(problems).to have(1).problems
-    end
-  end
-
-  context "global includes" do
-    let(:code) { "class test { file { 'file': } } \ninclude testclass" }
-
-    it "should detect a problem" do
-      expect(problems).to have(1).problems
-    end
-  end
-
+describe "global_function" do
   context "just a function" do
     let(:code) do
       <<-EOS
@@ -70,20 +28,6 @@ describe "global_resource" do
 
     it "should detect a problem" do
       expect(problems).to have(1).problems
-    end
-  end
-
-  context "global defaults" do
-    let(:code) do
-      <<-EOS
-      Exec {
-        path => '/usr/bin:/usr/sbin/:/bin:/sbin',
-      }
-      EOS
-    end
-
-    it "should not detect any problems" do
-      expect(problems).to have(0).problems
     end
   end
 end
