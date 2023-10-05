d69dc186808933e58560a5d5928d63ac5e053174
Daniel Wilhelm <daniel.wilhelm@nine.ch>
2023-10-05 14:24:55 +0000

    fix: rename file according to test content

--- spec/puppet-lint/plugins/check_global_function_spec.rb
+++ spec/puppet-lint/plugins/check_global_function_spec.rb
@@ -1,59 +1,33 @@
 require "spec_helper"
 
-describe "global_resource" do
-  context "just a class" do
-    let(:code) { "class test { file { 'file': } }" }
-
-    it "should not detect any problems" do
-      expect(problems).to have(0).problems
-    end
-  end
-
-  context "just a define" do
-    let(:code) { "define test ($param = undef) { file { 'file': } }" }
-
-    it "should not detect any problems" do
-      expect(problems).to have(0).problems
-    end
-  end
-
-  context "allow node resources" do
-    let(:code) { "node 'test' { file { 'file': } }" }
-
-    it "should not detect any problems" do
-      expect(problems).to have(0).problems
-    end
-  end
-
-  context "global file" do
-    let(:code) do
-      "file { 'file': } define test ($param = undef) { file { 'file': } }"
-    end
-
-    it "should detect a problem" do
-      expect(problems).to have(1).problems
-    end
-  end
-
-  context "global includes" do
-    let(:code) { "class test { file { 'file': } } \ninclude testclass" }
-
-    it "should detect a problem" do
-      expect(problems).to have(1).problems
-    end
-  end
-
-  context "global defaults" do
+describe "global_function" do
+  context "just a function" do
     let(:code) do
       <<-EOS
-      Exec {
-        path => '/usr/bin:/usr/sbin/:/bin:/sbin',
+      class test {
+        ensure_packages(['wordpress']);
       }
       EOS
     end
 
     it "should not detect any problems" do
       expect(problems).to have(0).problems
+    end
+  end
+
+  context "global functions" do
+    let(:code) do
+      <<-EOS
+      class test {
+        ensure_packages(['wordpress']);
+      }
+
+      ensure_packages(['wordpress']);
+      EOS
+    end
+
+    it "should detect a problem" do
+      expect(problems).to have(1).problems
     end
   end
 end
